278b0a9d0180d9ac4bcf9e2bb2bd50595ee07e4d
Brittany Chiang <brittany.chiang@gmail.com>
2018-10-11 23:35:24 -0400

    Delete stray stylus files and move skip to content styles

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
+import { theme } from '../styles';
+
+const SkipLink = styled.a`
+  position: absolute;
+  top: -1000px;
+  left: -1000px;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  border: 2px solid ${theme.colors.yellow};
+  padding: 10px 15px;
+  font-size: 16px;
+  background: #000;
+
+  &:active,
+  &:focus,
+  &:hover {
+    top: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+    width: auto;
+    height: auto;
+    overflow: visible;
+  }
+`;
 
 class Page extends React.Component {
   render() {
@@ -9,7 +36,7 @@ class Page extends React.Component {
 
     return (
       <div className="page">
-        <a href="#main" className="skip-link">Skip to content</a>
+        <SkipLink href="#main">Skip to content</SkipLink>
         <Header />
         {children}
         <Footer />
